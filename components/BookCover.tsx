1c2e7178d573dd4490870cbcaffa49f820af22fb
seoywa <132980713+seoywa@users.noreply.github.com>
2025-06-09 15:40:37 +0700

    Database seed

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,7 +1,9 @@
+'use client'
+
 import { cn } from "@/lib/utils";
-import Image from "next/image";
-import React from "react";
 import BookCoverSvg from "./BookCoverSvg";
+import { IKImage } from "imagekitio-next";
+import config from "@/lib/config";
 
 const variantStyles: Record<BookCoverVariant, string> = {
   extraSmall: "book-cover_extra_small",
@@ -41,11 +43,14 @@ const BookCover = ({
         className="absolute z-10"
         style={{ left: "12%", width: "87.5%", height: "88%" }}
       >
-        <Image
-          src={coverImage}
+        <IKImage
+          path={coverImage}
+          urlEndpoint={config.env.imagekit.urlEndpoint}
           alt="book-cover"
           fill
           className="rounded-sm object-fill"
+          loading="lazy"
+          lqip={{ active: true }}
         />
       </div>
     </div>
